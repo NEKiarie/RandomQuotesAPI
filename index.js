db4cf432101502774a9c84df4573378e5ef9139a
Elvis <s49188705@gmail.com>
2022-09-05 09:52:59 +0300

    Index.js file modified

--- index.js
+++ index.js
@@ -39,8 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //Speech out the quoteText on a button click
   //SpeechSynthesisUtterance is web speech API method that represent a speech request
   soundButton.addEventListener("click", () => {
-    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} Quote By: ${characterName.innerText}`
-    );
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} Quote By: ${characterName.innerText}`);
     speechSynthesis.speak(utterance); 
   });
 
